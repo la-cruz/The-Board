00a9e79477850be1c46306d1f06e66bfe9d250b7
AUDART <lucas.audart@etu.univ-lyon1.fr>
2020-11-07 15:08:18 +0100

    Adding other store config

--- client/store/index.js
+++ client/store/index.js
@@ -2,16 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/index';
 import propagateSocket from '../middleware/propagateSocket';
 
-const middlewareEnhancer = applyMiddleware(propagateSocket);
-
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  rootReducer,
-  compose(
-    middlewareEnhancer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(propagateSocket),
+));
 /* eslint-enable */
 
 export default store;
